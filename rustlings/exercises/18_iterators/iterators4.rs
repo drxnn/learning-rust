73ad7d49c35160cc551dc4f8253d8436fb19be7c
Drin Bratotini <drinbratotini@Drins-MacBook-Pro.local>
2025-08-14 16:32:23 -0400

    iterators 4

--- rustlings/exercises/18_iterators/iterators4.rs
+++ rustlings/exercises/18_iterators/iterators4.rs
@@ -1,15 +1,8 @@
 fn factorial(num: u64) -> u64 {
-    // TODO: Complete this function to return the factorial of `num` which is
-    // defined as `1 * 2 * 3 * … * num`.
-    // https://en.wikipedia.org/wiki/Factorial
-    //
-    // Do not use:
-    // - early returns (using the `return` keyword explicitly)
-    // Try not to use:
-    // - imperative style loops (for/while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
+    match num {
+        0 => 1,
+        _ => (1..num + 1).rev().fold(1, |acc, n| acc * n),
+    }
 }
 
 fn main() {
